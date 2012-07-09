43123d58c7d50cf18effd0850efd5ecaa7c181a0
Adam Brenecki <adam@brenecki.id.au>
2012-07-09 15:37:30 +0930

    Add README and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mzevents',
+	version='0.1pre',
+	description='Event pages for the Mezzanine CMS',
+	author='Adam Brenecki',
+	author_email='abrenecki@sbtc.org.au',
+	url='',
+	packages=['mzevents'],
+)
